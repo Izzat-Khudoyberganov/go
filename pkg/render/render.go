765abe9d335e1c5899ffe61472429d077452776d
izzat <izzat@gmail.com>
2024-10-09 00:08:43 +0500

    added dynamic template caching

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,16 +2,59 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
 		fmt.Println("error parsing template", err)
 		return
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplateTest(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tc[t]
+
+	if !inMap {
+		err = createTemplateCache(t)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.template.tmpl",
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	tc[t] = tmpl
+
+	return nil
 }
