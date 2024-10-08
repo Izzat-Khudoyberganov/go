8ae84931eef0ea14a82ea3236ca937273f2ad142
izzat <izzat@gmail.com>
2024-10-08 23:27:27 +0500

    separated files to modules

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
+	"goo/pkg/handlers"
 	"net/http"
 )
 
 const portname = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting application on port %s", portname)
 	http.ListenAndServe(portname, nil)
