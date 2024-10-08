6b38a72e0428d5372addbde4264ff39faf4807b9
izzat <izzat@gmail.com>
2024-10-08 23:12:19 +0500

    added .tmpl

--- main.go
+++ main.go
@@ -7,24 +7,10 @@ import (
 
 const portname = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is home page")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	sum := AddValues(2, 2)
-	_, _ = fmt.Fprintf(w, "This is about page and 2 + 2 = %d", sum)
-
-}
-
-func AddValues(x, y int) int {
-	return x + y
-}
-
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Printf("Starting application n port %s", portname)
+	fmt.Printf("Starting application on port %s", portname)
 	http.ListenAndServe(portname, nil)
 }
